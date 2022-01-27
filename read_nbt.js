4724aa97087090b7345169d26b35b237054ae6e0
Jacky Xie <jackyxie2520@outlook.com>
2022-01-26 21:03:27 -0500

    Use canvas to display map data

--- read_nbt.js
+++ read_nbt.js
@@ -1,6 +1,34 @@
 window.onload = function() {
 
 document.getElementById('fileSelect').addEventListener('change', loadFile);
+canvas = document.getElementById("mapCanvas");
+ctx = canvas.getContext("2d");
+var width = 128 * 8;
+var height = 128 * 8;
+canvas.width = width;
+canvas.height = height;
+
+// https://stackoverflow.com/questions/3448347/how-to-scale-an-imagedata-in-html-canvas user3079576
+function scaleImageData(imageData, scale) {
+	var scaled = ctx.createImageData(imageData.width * scale, imageData.height * scale);
+	var subLine = ctx.createImageData(scale, 1).data
+	for (var row = 0; row < imageData.height; row++) {
+			for (var col = 0; col < imageData.width; col++) {
+					var sourcePixel = imageData.data.subarray(
+							(row * imageData.width + col) * 4,
+							(row * imageData.width + col) * 4 + 4
+					);
+					for (var x = 0; x < scale; x++) subLine.set(sourcePixel, x*4)
+					for (var y = 0; y < scale; y++) {
+							var destRow = row * scale + y;
+							var destCol = col * scale;
+							scaled.data.set(subLine, (destRow * scaled.width + destCol) * 4)
+					}
+			}
+	}
+
+	return scaled;
+}
 
 function loadFile(event){
 	var file = event.target.files[0];
@@ -10,23 +38,23 @@ function loadFile(event){
 			try {
 				var parser = new nbtParser.NBTParser();
 				var result = parser.parse(e.target.result);
-				mapColorArray = result.data.colors;
-	
-				output = document.getElementById('map');
-				output.innerHTML = "";
+				mapData = result.data.colors;
 
-				s = "<table>"
-				for (var i=0; i<128; i++) {
-					s += "<tr>"
-					for (var j=0; j<128; j++) {
-						let val = mapColorArray[i*128 + j];
-						s += ("<td style=\"background-color:" + rgbColorTable[val] + "\">" + " "+ "</td>");
+				var imgData = ctx.createImageData(128, 128);
+				var pixels = imgData.data;
+
+				for (var i=0; i<16384; i++) {
+					let val = mapData[i];
+					if (val < 0) {
+						val = 0;
 					}
-					s += "</tr>";
+					pixels[i*4] = colorLUT[val][0];
+					pixels[i*4+1] = colorLUT[val][1];
+					pixels[i*4+2] = colorLUT[val][2];
+					pixels[i*4+3] = colorLUT[val][3];
 				}
-				s += "</table>"
-				output.innerHTML += s;
 
+				ctx.putImageData(scaleImageData(imgData, 8), 0, 0);
 			} catch(e) {
 					console.error(e)
 			}
