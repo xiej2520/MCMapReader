fa718702c2d0f2855cebba0c6e8309bbbfafbbc8
Jacky Xie <jackyxie2520@outlook.com>
2022-01-29 00:17:57 -0500

    Added functionality for loading multiple maps at once and scrolling through them

--- read_nbt.js
+++ read_nbt.js
@@ -1,12 +1,75 @@
 window.onload = function() {
 
-document.getElementById('fileSelect').addEventListener('change', loadFile);
 canvas = document.getElementById("mapCanvas");
 ctx = canvas.getContext("2d");
 var width = 128 * 8;
 var height = 128 * 8;
 canvas.width = width;
 canvas.height = height;
+
+var fileSelect = document.getElementById("fileSelect");
+fileSelect.addEventListener("change", addFiles);
+
+var fileList = [];
+var currentIndex = 0;
+
+var mapLeftButton = document.getElementById("mapLeft");
+var mapRightButton = document.getElementById("mapRight");
+mapLeftButton.addEventListener("click", function() {moveMap("left")});
+mapRightButton.addEventListener("click", function() {moveMap("right")});
+
+var currentMapText = document.getElementById("currentMap");
+var totalMapText = document.getElementById("totalMaps");
+
+
+function moveMap(dir) {
+	if (dir == "left" && currentIndex > 0) {
+		paintMap(fileList[--currentIndex]);
+		currentMapText.innerHTML = currentIndex+1;
+	}
+	else if (dir == "right" && currentIndex < fileList.length-1) {
+		paintMap(fileList[++currentIndex]);
+		currentMapText.innerHTML = currentIndex+1;
+	}
+
+}
+
+function addFiles(event) {
+	for (var i=0; i<event.target.files.length; i++) {
+		fileList.push(event.target.files[i]);
+	}
+	currentIndex = fileList.length-1;
+	currentMapText.innerHTML = fileList.length;
+	paintMap(fileList[currentIndex]);
+	totalMapText.innerHTML = fileList.length;
+}
+
+
+function paintMap(file) {
+	let r = new FileReader();
+	r.onload = function(event) {
+		var parser = new nbtParser.NBTParser();
+		var result = parser.parse(event.target.result);
+		mapData = result.data.colors;
+
+		var imgData = ctx.createImageData(128, 128);
+		var pixels = imgData.data;
+
+		for (var i=0; i<16384; i++) {
+			let val = mapData[i];
+			if (val < 0) {
+				val = 0;
+			}
+			pixels[i*4] = colorLUT[val][0];
+			pixels[i*4+1] = colorLUT[val][1];
+			pixels[i*4+2] = colorLUT[val][2];
+			pixels[i*4+3] = colorLUT[val][3];
+		}
+		ctx.putImageData(scaleImageData(imgData, 8), 0, 0);
+	}
+	r.readAsArrayBuffer(file); 
+}
+
 
 // https://stackoverflow.com/questions/3448347/how-to-scale-an-imagedata-in-html-canvas user3079576
 function scaleImageData(imageData, scale) {
@@ -28,40 +91,5 @@ function scaleImageData(imageData, scale) {
 	}
 
 	return scaled;
-}
-
-function loadFile(event){
-	var file = event.target.files[0];
-	if (file) {
-		let r = new FileReader();
-		r.onload = function(e) {
-			try {
-				var parser = new nbtParser.NBTParser();
-				var result = parser.parse(e.target.result);
-				mapData = result.data.colors;
-
-				var imgData = ctx.createImageData(128, 128);
-				var pixels = imgData.data;
-
-				for (var i=0; i<16384; i++) {
-					let val = mapData[i];
-					if (val < 0) {
-						val = 0;
-					}
-					pixels[i*4] = colorLUT[val][0];
-					pixels[i*4+1] = colorLUT[val][1];
-					pixels[i*4+2] = colorLUT[val][2];
-					pixels[i*4+3] = colorLUT[val][3];
-				}
-
-				ctx.putImageData(scaleImageData(imgData, 8), 0, 0);
-			} catch(e) {
-					console.error(e)
-			}
-		}
-		r.readAsArrayBuffer(file); 
-	} else {
-			console.error("Failed to load file");
-	}
 }
 }
